03d3a32dd1349fc973e037c43280f22d45cb2846
Joschi <joschka.strueber@uni-muenster.de>
2021-11-10 18:30:56 +0100

    implement actions_to_regs, scores_to_action and basic network

--- network.py
+++ network.py
@@ -13,6 +13,32 @@ class ClassificationNetwork(torch.nn.Module):
         # setting device on GPU if available, else CPU
         device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
+        self.cnn = torch.nn.Sequential(
+            torch.nn.Conv2d(3, 32, kernel_size=3, stride=2, padding=1),
+            #torch.nn.BatchNorm2d(32),
+            torch.nn.LeakyReLU(negative_slope=0.2),
+            torch.nn.Conv2d(32, 48, kernel_size=3, stride=2, padding=1),
+            #torch.nn.BatchNorm2d(48),
+            torch.nn.LeakyReLU(negative_slope=0.2),
+            torch.nn.Conv2d(48, 64, kernel_size=3, stride=2, padding=1),
+            torch.nn.BatchNorm2d(64),
+            torch.nn.LeakyReLU(negative_slope=0.2),
+        )
+        self.mlp = torch.nn.Sequential(
+            torch.nn.Linear(64, 128),
+            torch.nn.Dropout(p=0.3),
+            torch.nn.LeakyReLU(negative_slope=0.2),
+            torch.nn.Linear(128, 64),
+            torch.nn.Dropout(p=0.3),
+            torch.nn.LeakyReLU(negative_slope=0.2),
+            torch.nn.Linear(64, 2),
+            torch.nn.Tanh()
+        )
+
+        if torch.cuda.is_available():
+            self.cnn = self.cnn.cuda()
+            self.mlp = self.mlp.cuda()
+
     def forward(self, observation):
         """
         1.1 e)
@@ -21,7 +47,14 @@ class ClassificationNetwork(torch.nn.Module):
         observation:   torch.Tensor of size (batch_size, 96, 96, 3)
         return         torch.Tensor of size (batch_size, C)
         """
-        pass
+        x = observation/255.
+        x = x.permute(0, 3, 1, 2)
+        x = self.cnn(x)
+        x = torch.mean(x, (2, 3))  # global average pooling
+        x = self.mlp(x)
+        #x = x * torch.Tensor([1.1, 1.]).cuda()  # eliminate steering sensitivity
+        #x = torch.clip(x, -1, 1)
+        return x
 
     def actions_to_classes(self, actions):
         """
@@ -34,16 +67,54 @@ class ClassificationNetwork(torch.nn.Module):
         """
         pass
 
+    def actions_to_regs(self, actions):
+        """
+        1.2 c)
+        For a given set of actions map every action to its corresponding
+        action-regression representation. Every action is represented by a 2-dim vector
+        with the first entry being the steering and the second being the gas/
+        braking.
+        The first entry of the action is in {-1, 0, 1}, it can remain unchanged.
+        The second is the gas in {0, 0.5} and the third braking in {0, 0.8}.
+        We map them to a combined value in {-1, 0, 1}.
+        actions:        python list of N torch.Tensors of size 3
+        return          python list of N torch.Tensors of size 2
+        """
+        reg_output = [torch.zeros(2) for a in actions]
+        for i, a in enumerate(actions):
+            # add steering
+            reg_output[i][0] = a[0]
+
+            # scale gas and braking to [-1, 1]
+            if a[2] > 0:
+                reg_output[i][1] = -1
+            elif a[1] > 0:
+                reg_output[i][1] = 1
+
+        return reg_output
+
     def scores_to_action(self, scores):
         """
-        1.1 c)
+        1.1 c) / 1.2 c)
         Maps the scores predicted by the network to an action-class and returns
         the corresponding action [steer, gas, brake].
                         C = number of classes
-        scores:         python list of torch.Tensors of size C
+        scores:         python list of torch.Tensors of size 2
         return          (float, float, float)
         """
-        pass
+        scores = scores.cpu().detach()
+        score = scores[0]
+        steering = score[0].item()
+        gas, brake = 0., 0.
+
+        long_contrl = score[1].item()
+        if long_contrl > 0.:
+            gas = long_contrl * 0.5
+            brake = 0.
+        elif long_contrl < 0.:
+            gas = 0.
+            brake = long_contrl * 0.8
+        return steering, gas, brake
 
     def extract_sensor_values(self, observation, batch_size):
         """
