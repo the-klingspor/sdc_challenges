cb8d7cfc798c3191b43a216fd1451c1e28787061
lennytubby <lennart@slusny.de>
2021-12-07 17:58:05 +0100

    learning, first test

--- learning.py
+++ learning.py
@@ -28,24 +28,26 @@ def perform_qlearning_step(policy_net, target_net, optimizer, replay_buffer, bat
     """
  
     # 1. Sample transitions from replay_buffer
-    transitions = replay_buffer.sample(batch_size)
+    [obs_batch, act_batch, rew_batch, next_obs_batch, done_mask] = replay_buffer.sample(batch_size)
 
     # 2. Compute Q(s_t, a)
-    prediction = policy_net(transitions)
+    prediction = policy_net(obs_batch)[np.arange(batch_size),act_batch]
     
     # 3. Compute \max_a Q(s_{t+1}, a) for all next states.
-    MaxQ = max(target_net())
+    MaxQ = np.amax(target_net(obs_batch),axis=1)
+
     # 4. Mask next state values where episodes have terminated
 
 
     # 5. Compute the target
-    target = reward + gamma*MaxQ
+    target = rew_batch + gamma*MaxQ
 
     # 6. Compute the loss
     Loss = (target - prediction)**2
+    print(Loss)
 
     # 7. Calculate the gradients
-    stochastic gradient descent
+    #stochastic gradient descent
 
     # 8. Clip the gradients
 
